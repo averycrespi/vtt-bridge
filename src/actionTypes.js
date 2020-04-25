50ff0f84cc1117be2de620495d89a9681508dca1
Avery Crespi <averycrespi@gmail.com>
2020-04-25 16:01:06 -0700

    Hook saving throws, initiative, and weapons

---
+++ src/actionTypes.js
@@ -0,0 +1,5 @@
+export const ABILITY_SCORE = "ABILITY_SCORE";
+export const SKILL = "SKILL";
+export const SAVING_THROW = "SAVING_THROW";
+export const INITIATIVE = "INITIATIVE";
+export const WEAPON = "WEAPON";
--- src/scripts/dmv.js
+++ src/scripts/dmv.js
@@ -1,6 +1,12 @@
-console.log("Loaded dmv.js");
-document.body.style.border = "5px solid red";
+import {
+  ABILITY_SCORE,
+  INITIATIVE,
+  SAVING_THROW,
+  SKILL,
+  WEAPON,
+} from "../actionTypes";
 
+// Wait for the page to finish loading.
 const interval = setInterval(() => {
   if (document.getElementsByClassName("character-name").length > 0) {
     clearInterval(interval);
@@ -9,34 +15,105 @@ const interval = setInterval(() => {
 }, 100);
 
 const onLoad = () => {
+  console.log("Loaded dmv.js");
   hookAbilityScores();
   hookSkills();
+  hookSavingThrows();
+  hookInitiative();
+  hookWeapons();
 };
 
-const onClick = (payload) => alert(JSON.stringify(payload));
+const dispatch = (type, payload) => alert(JSON.stringify(payload));
+
+const createRollButton = (onclick) => {
+  const button = document.createElement("button");
+  button.innerText = "Roll";
+  button.className = "form-button";
+  button.onclick = onclick;
+  return button;
+};
 
 const hookAbilityScores = () => {
-  const root = document.querySelector(".ability-scores");
-  for (const child of root.children) {
-    const name = child.querySelector("span").innerText;
-    const score = child.querySelector(".ability-score").innerText;
-    const modifier = child.querySelector(".ability-score-modifier").innerText;
-    child.addEventListener("click", function () {
-      onClick({ name, score, modifier });
+  const parent = document.querySelector(".ability-scores");
+  for (const child of parent.children) {
+    const button = createRollButton(function () {
+      dispatch(ABILITY_SCORE, {
+        name: child.querySelector(".ability-score-name").innerText,
+        score: child.querySelector(".ability-score").innerText,
+        modifier: child.querySelector(".ability-score-modifier").innerText,
+      });
     });
+    child.appendChild(button);
   }
   console.log("Hooked ability scores");
 };
 
 const hookSkills = () => {
-  const root = document.querySelector(".skills");
-  const rows = root.querySelectorAll("tr");
+  const table = document.querySelector(".skills");
+  const rows = table.querySelectorAll("tr");
   for (const row of rows) {
-    const name = row.querySelector("span").innerText;
-    const bonus = row.querySelector(".skillbonus").innerText;
-    row.addEventListener("click", function () {
-      onClick({ name, bonus });
+    const cell = document.createElement("td");
+    const button = createRollButton(function () {
+      dispatch(SKILL, {
+        name: row.querySelector(".skill-name").innerText,
+        bonus: row.querySelector(".skillbonus").innerText,
+      });
     });
+    cell.appendChild(button);
+    row.appendChild(cell);
   }
   console.log("Hooked skills");
+};
+
+const hookSavingThrows = () => {
+  // This table has no class, so we need to search upwards.
+  const table = document.querySelector(".saving-throw-name").closest("table");
+  const rows = table.querySelectorAll("tr");
+  for (const row of rows) {
+    const cell = document.createElement("td");
+    const button = createRollButton(function () {
+      dispatch(SAVING_THROW, {
+        name: row.querySelector(".saving-throw-name").innerText,
+        bonus: row.querySelector(".saving-throw-bonus").innerText,
+      });
+    });
+    cell.appendChild(button);
+    row.appendChild(cell);
+  }
+  console.log("Hooked saving throws");
+};
+
+const hookInitiative = () => {
+  const elem = document.querySelector(".initiative");
+  const button = createRollButton(function () {
+    dispatch(INITIATIVE, {
+      bonus: elem.innerText,
+    });
+  });
+  elem.parentNode.appendChild(button);
+  console.log("Hooked initiative");
+};
+
+const hookWeapons = () => {
+  const table = document.querySelector(".weapons");
+  const rows = table.querySelectorAll("tr");
+  for (const row of rows) {
+    if (row.querySelectorAll("th").length > 0) {
+      // Don't add a button to the header.
+      continue;
+    }
+    const cell = document.createElement("td");
+    const button = createRollButton(function (e) {
+      // Don't expand the section when the button is clicked.
+      event.stopPropagation();
+      dispatch(WEAPON, {
+        name: row.querySelector(".weapon").innerText,
+        details: row.querySelector(".weapon-damage-modifier").innerText,
+        attackModifier: row.querySelector(".attack-modifier").innerText,
+      });
+    });
+    cell.appendChild(button);
+    row.appendChild(cell);
+  }
+  console.log("Hooked weapons");
 };
