c6bfaa7f400bce30730c2efa0649bc635ccb111c
Avery Crespi <averycrespi@gmail.com>
2020-04-26 12:03:47 -0700

    Inject rendered messages into Roll20

--- src/scripts/roll20.js
+++ src/scripts/roll20.js
@@ -4,6 +4,12 @@ const handleMessage = (message) => {
   console.log(
     "Received message from background.js: " + JSON.stringify(message)
   );
+  const chat = document.querySelector("#textchat-input");
+  const textArea = chat.querySelector("textarea");
+  textArea.value = message;
+  setTimeout(function () {
+    chat.querySelector(".btn").click();
+  }, 100);
 };
 
 browser.runtime.onMessage.addListener(handleMessage);
