7baff7d12d87339008895f283c0c7282c244ff90
Avery Crespi <averycrespi@gmail.com>
2020-04-27 15:21:48 -0700

    Add connection notification to Roll20

--- src/scripts/roll20.js
+++ src/scripts/roll20.js
@@ -9,3 +9,18 @@ browser.runtime.onMessage.addListener((commands) =>
     chat.querySelector(".btn").click();
   })
 );
+
+onElementLoad("#textchat", () => {
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("message", "system");
+
+  const spacer = document.createElement("div");
+  spacer.classList.add("spacer");
+  wrapper.appendChild(spacer);
+
+  const message = document.createElement("strong");
+  message.innerText = "Connected to Dungeon Master's Vault.";
+  wrapper.append(message);
+
+  document.querySelector("#textchat").appendChild(wrapper);
+});
