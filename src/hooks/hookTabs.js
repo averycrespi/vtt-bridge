9b338d51de8b67da389dffbe9a5930f847b65f04
Avery Crespi <averycrespi@gmail.com>
2020-04-27 14:16:47 -0700

    Refactor tab hooks into hookTabs

---
+++ src/hooks/hookTabs.js
@@ -0,0 +1,62 @@
+import hookActions from "./hookActions";
+import hookFeatures from "./hookFeatures";
+import hookInitiative from "./hookInitiative";
+import hookWeapons from "./hookWeapons";
+import { onElementLoad } from "../common/dom";
+
+// Global variable for tracking the active tab.
+let activeTab = 0;
+
+const hookTabs = (onClick) => {
+  const [
+    combatTab,
+    proficienciesTab,
+    spellsTab,
+    featuresTab,
+    equipmentTab,
+  ] = document.querySelectorAll(".flex-grow-1.t-a-c");
+
+  combatTab.addEventListener("click", () => {
+    if (activeTab !== 0) {
+      console.debug("Switched to combat tab");
+      hookInitiative(onClick);
+      hookWeapons(onClick);
+    }
+    activeTab = 0;
+  });
+
+  proficienciesTab.addEventListener("click", () => {
+    if (activeTab !== 1) {
+      console.debug("Switched to proficiencies tab");
+    }
+    activeTab = 1;
+  });
+
+  spellsTab.addEventListener("click", () => {
+    if (activeTab !== 2) {
+      console.debug("Switched to spells tab");
+      //TODO: hook cantrips and spells
+    }
+    activeTab = 2;
+  });
+
+  featuresTab.addEventListener("click", () => {
+    if (activeTab !== 3) {
+      console.debug("Switched to features tab");
+      hookActions(onClick);
+      hookFeatures(onClick);
+    }
+    activeTab = 3;
+  });
+
+  equipmentTab.addEventListener("click", () => {
+    if (activeTab !== 4) {
+      console.debug("Switched to equipment tab");
+      hookWeapons(onClick);
+    }
+    activeTab = 4;
+  });
+};
+
+export default (onClick) =>
+  onElementLoad(".initiative", () => hookTabs(onClick));
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,11 +2,10 @@ import { createButton, onElementLoad } from "../common/dom";
 
 import { LEFT_MARGIN } from "../common/classes";
 import hookAbilityScores from "./hookAbilityScores";
-import hookActions from "./hookActions";
-import hookFeatures from "./hookFeatures";
 import hookInitiative from "./hookInitiative";
 import hookSavingThrows from "./hookSavingThrows";
 import hookSkills from "./hookSkills";
+import hookTabs from "./hookTabs";
 import hookWeapons from "./hookWeapons";
 
 export const createHooks = (onClick) => {
@@ -23,65 +22,11 @@ export const createHooks = (onClick) => {
         // Initiative and weapons are loaded in the default tab.
         hookInitiative(onClick);
         hookWeapons(onClick);
-        // Tabs require additional logic to recreate hooks on switch.
-        createTabHooks(onClick);
+        // Recreate hooks on tab switch.
+        hookTabs(onClick);
       },
-      [LEFT_MARGIN, "vtt-connect-button"]
+      [LEFT_MARGIN]
     );
     parent.appendChild(button);
-  });
-};
-
-// Global variable for tracking the active tab.
-let activeTab = 0;
-
-const createTabHooks = (onClick) => {
-  const [
-    combatTab,
-    proficienciesTab,
-    spellsTab,
-    featuresTab,
-    equipmentTab,
-  ] = document.querySelectorAll(".flex-grow-1.t-a-c");
-
-  combatTab.addEventListener("click", () => {
-    if (activeTab !== 0) {
-      console.debug("Switched to combat tab");
-      hookInitiative(onClick);
-      hookWeapons(onClick);
-    }
-    activeTab = 0;
-  });
-
-  proficienciesTab.addEventListener("click", () => {
-    if (activeTab !== 1) {
-      console.debug("Switched to proficiencies tab");
-    }
-    activeTab = 1;
-  });
-
-  spellsTab.addEventListener("click", () => {
-    if (activeTab !== 2) {
-      console.debug("Switched to spells tab");
-      //TODO: hook cantrips and spells
-    }
-    activeTab = 2;
-  });
-
-  featuresTab.addEventListener("click", () => {
-    if (activeTab !== 3) {
-      console.debug("Switched to features tab");
-      hookActions(onClick);
-      hookFeatures(onClick);
-    }
-    activeTab = 3;
-  });
-
-  equipmentTab.addEventListener("click", () => {
-    if (activeTab !== 4) {
-      console.debug("Switched to equipment tab");
-      hookWeapons(onClick);
-    }
-    activeTab = 4;
   });
 };
