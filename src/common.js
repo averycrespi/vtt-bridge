da9cfd25c6aec23df523b020f3c615f1341a5c48
Avery Crespi <averycrespi@gmail.com>
2020-04-30 08:31:33 -0700

    Refactor onElementLoad into onPredicate

--- src/common.js
+++ src/common.js
@@ -1,29 +1,38 @@
 /**
- * Run a callback after an element loads.
+ * Run a callback after a predicate is satisfied.
  *
  * Uses exponential backoff to improve performance.
  *
- * @param {String} selector
+ * @param {Function} predicate
  * @param {Function} callback
  * @param {Number} attempts
  * @param {Number} timeout
  */
-export const onElementLoad = (
-  selector,
+export const onPredicate = (
+  predicate,
   callback,
-  attempts = 10,
+  attempts = 5,
   timeout = 100
 ) => {
-  if (document.querySelector(selector)) {
+  if (predicate()) {
     callback();
   } else if (attempts <= 0) {
-    console.warn("Maximum number of attempts exceeded: " + selector);
+    console.warn("Maximum number of attempts exceeded");
   } else {
     setTimeout(function () {
-      onElementLoad(selector, callback, attempts - 1, timeout * 2);
+      onPredicate(predicate, callback, attempts - 1, timeout * 2);
     }, timeout);
   }
 };
+
+/**
+ * Run a callback after an element loads.
+ *
+ * @param {String} selector
+ * @param {Function} callback
+ */
+export const onElementLoad = (selector, callback) =>
+  onPredicate(() => !!document.querySelector(selector), callback);
 
 /**
  * Enum of message types.
