5b5cd287b247d38e329977e57ea5cfe134b2a4dc
Avery Crespi <averycrespi@gmail.com>
2020-05-01 12:42:39 -0700

    Add close button to Roll20 notification
    Closes #20

---
+++ src/roll20/classes.js
@@ -0,0 +1,2 @@
+export const CLOSE_BUTTON = "vtt-close-button";
+export const NOTIFICATION = "vtt-notification";
--- src/roll20/createNotification.js
+++ src/roll20/createNotification.js
@@ -1,13 +1,15 @@
+import * as classes from "./classes";
+
 import { onElementLoad } from "../common";
 
 const ready = () => {
-  if (document.querySelector(".vtt-notification")) {
-    console.warn("Notification already exists");
+  if (document.querySelector("." + classes.NOTIFICATION)) {
+    // Notification already exists.
     return;
   }
 
   const notification = document.createElement("div");
-  notification.classList.add("message", "system", "vtt-notification");
+  notification.classList.add("message", "system", classes.NOTIFICATION);
 
   const spacer = document.createElement("div");
   spacer.classList.add("spacer");
@@ -16,6 +18,14 @@ const ready = () => {
   const text = document.createElement("strong");
   text.innerText = "Connected to Dungeon Master's Vault.";
   notification.append(text);
+
+  const button = document.createElement("button");
+  button.classList.add(classes.CLOSE_BUTTON);
+  button.innerText = "×";
+  button.onclick = function () {
+    notification.remove();
+  };
+  notification.append(button);
 
   const chat = document.querySelector("#textchat");
   chat.appendChild(notification);
