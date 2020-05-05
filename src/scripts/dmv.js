0baa93fbd889379c9fa33710d2c9bcc3076a4685
Avery Crespi <averycrespi@gmail.com>
2020-05-05 10:23:38 -0700

    Add warning toast about native roll buttons
    Temporary fix for #34

--- src/scripts/dmv.js
+++ src/scripts/dmv.js
@@ -10,6 +10,13 @@ let notyf = new Notyf({
   types: [{ type: "success", background: "#f1a20f" }],
 });
 
+notyf.error({
+  message:
+    "Dungeon Master's Vault has recently added native roll buttons, which may interfere with the VTT Bridge extension. This will be fixed in the next release.",
+  duration: 0,
+  dismissible: true,
+});
+
 addConnectToRoll20Button((commands) => {
   notyf.success("Sent commands!");
   browser.runtime.sendMessage({ type: messageTypes.ENQUEUE, commands });
