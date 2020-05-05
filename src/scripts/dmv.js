5212063513b603a68d141d80c1f580b123c8f09a
Avery Crespi <averycrespi@gmail.com>
2020-05-05 09:27:21 -0700

    Replaced custom toast with Notyf library
    Closes #32

--- src/scripts/dmv.js
+++ src/scripts/dmv.js
@@ -1,10 +1,16 @@
+import "notyf/notyf.min.css";
+
+import { Notyf } from "notyf";
 import { addConnectToRoll20Button } from "../dmv/buttons/connectToRoll20";
 import { messageTypes } from "../common";
-import { showToast } from "../dmv/toasts";
 
 console.debug("Loading dmv.js ...");
 
+let notyf = new Notyf({
+  types: [{ type: "success", background: "#f1a20f" }],
+});
+
 addConnectToRoll20Button((commands) => {
-  showToast();
+  notyf.success("Sent commands!");
   browser.runtime.sendMessage({ type: messageTypes.ENQUEUE, commands });
 });
