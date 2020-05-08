94ef0b24d39599e83a7e2887245c0e6e73f61c11
Avery Crespi <averycrespi@gmail.com>
2020-05-07 19:50:28 -0700

    Update emote and rollD20 command builders

--- src/dmv/commands.js
+++ src/dmv/commands.js
@@ -1,45 +1,58 @@
 // Does the roll have advantage or disadvantage?
-const hasAdvantage = (event) => event.ctrlKey;
-const hasDisadvantage = (event) => event.shiftKey;
+const hasAdvantage = (event) => event && event.ctrlKey;
+const hasDisadvantage = (event) => event && event.shiftKey;
 
-const eventToRoll = (event) => {
-  if (hasAdvantage(event)) {
-    return "/roll 2d20kh1";
+const emote = (text, event) => {
+  if (!event) {
+    return "/emote " + text;
+  } else if (hasAdvantage(event)) {
+    return "/emote " + text + " with advantage";
   } else if (hasDisadvantage(event)) {
-    return "/roll 2d20kl1";
+    return "/emote " + text + " with disadvantage";
   } else {
-    return "/roll 1d20";
+    return "/emote " + text;
   }
 };
 
-const rollD20 = (mod, event) => eventToRoll(event) + (mod === "0" ? "" : mod);
+const rollD20 = (mod, event = null) => {
+  const safeMod = mod === "0" ? "" : mod;
+  if (!event) {
+    return "/roll 1d20" + safeMod;
+  } else if (hasAdvantage(event)) {
+    return "/roll 2d20kh1" + safeMod;
+  } else if (hasDisadvantage(event)) {
+    return "/roll 2d20kl1" + safeMod;
+  } else {
+    return "/roll 1d20" + safeMod;
+  }
+};
 
 export const attackWith = (name, mod, event) => ({
   toast: "Attacked with " + name + "!",
-  commands: ["/em attacks with " + name, rollD20(mod, event)],
+  commands: [emote("attacks with " + name, event), rollD20(mod, event)],
 });
 
 export const rollCheck = (name, mod, event) => ({
   toast: "Rolled " + name + " check!",
-  commands: ["/em rolls " + name + " check", rollD20(mod, event)],
+  commands: [emote("rolls " + name + " check", event), rollD20(mod, event)],
 });
 
 export const rollDamage = (name, damage) => ({
   toast: "Rolled " + name + " damage!",
-  commands: ["/em rolls " + name + " damage", "/roll " + damage],
+  commands: [emote("rolls " + name + " damage"), "/roll " + damage],
 });
 
 export const rollFor = (name, mod, event) => ({
   toast: "Rolled " + name + "!",
-  commands: ["/em rolls " + name, rollD20(mod, event)],
+  commands: [emote("rolls " + name, event), rollD20(mod, event)],
 });
 
 export const rollSave = (name, mod, event) => ({
   toast: "Rolled " + name + " save!",
-  commands: ["/em rolls " + name + " save", rollD20(mod, event)],
+  commands: [emote("rolls " + name + " save", event), rollD20(mod, event)],
 });
 
 export const useFeature = (name, description) => ({
   toast: "Used " + name + "!",
-  commands: ["/em uses " + name, description],
+  commands: [emote("uses " + name), description],
 });
