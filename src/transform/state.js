081d4bc31a314c3f260aa1ea98b80334ec80b150
Avery Crespi <averycrespi@gmail.com>
2020-06-23 16:02:33 -0700

    Expand unit test suite (#75)
    * Add unit tests for state.js
    
    * Add property checking to state.js
    
    Closes #72

--- src/transform/state.js
+++ src/transform/state.js
@@ -16,43 +16,60 @@ export const parseState = (state) => {
   switch (className) {
     case classes.attackWithSpell:
     case classes.attackWithWeapon:
+      validate(name, mod);
       return {
         toast: makeToast(character + " attacked with " + name, options),
         commands: [makeEmote(character + " attacks with " + name, options), makeD20Roll(mod, options)],
       };
     case classes.castSpell:
+      validate(name, description);
       return {
         toast: makeToast(character + " cast " + name, options),
         commands: [makeEmote(character + " casts " + name, options), makeDescription(description, options)],
       };
     case classes.rollAbilityScore:
     case classes.rollSkill:
+      validate(name, mod);
       return {
         toast: makeToast(character + " rolled " + name + " check", options),
         commands: [makeEmote(character + " rolls " + name + " check", options), makeD20Roll(mod, options)],
       };
     case classes.rollInitiative:
     case classes.rollProficiency:
+      validate(name, mod);
       return {
         toast: makeToast(character + " rolled " + name, options),
         commands: [makeEmote(character + " rolls " + name, options), makeD20Roll(mod, options)],
       };
     case classes.rollSavingThrow:
+      validate(name, mod);
       return {
         toast: makeToast(character + " rolled " + name + " save", options),
         commands: [makeEmote(character + " rolls " + name + " save", options), makeD20Roll(mod, options)],
       };
     case classes.rollWeaponDamage:
+      validate(name, damage);
       return {
         toast: makeToast(character + " rolled " + name + " damage", options),
         commands: [makeEmote(character + " rolls " + name + " damage", options), makeDamageRoll(damage, options)],
       };
     case classes.useFeature:
+      validate(name, description);
       return {
         toast: makeToast(character + " used " + name, options),
         commands: [makeEmote(character + " uses " + name, options), makeDescription(description, options)],
       };
     default:
       throw "Unknown class name: " + className;
+  }
+};
+
+const validate = (...vs) => {
+  for (const v in vs) {
+    if (v === undefined) {
+      throw "Undefined variable in: " + vs.toString();
+    } else if (v === "") {
+      throw "Empty string in: " + vs.toString();
+    }
   }
 };
---
+++ src/transform/state.test.js
@@ -0,0 +1,37 @@
+import { classes } from "../common";
+import { parseState } from "./state";
+
+describe("parse state", () => {
+  test("Player rolls STR check with advantage", () => {
+    const state = {
+      click: { className: classes.rollAbilityScore, event: { ctrlKey: true }, data: { name: "STR", mod: "+1" } },
+      visible: true,
+      character: "Player",
+    };
+    const { toast, commands } = parseState(state);
+    expect(toast).toBe("Player rolled STR check with advantage!");
+    expect(commands).toStrictEqual(["/em : Player rolls STR check with advantage", "/roll 2d20kh1+1"]);
+  });
+
+  test("Wizard rolls initiative with disadvantage", () => {
+    const state = {
+      click: { className: classes.rollInitiative, event: { shiftKey: true }, data: { mod: "0", name: "initiative" } },
+      visible: true,
+      character: "Wizard",
+    };
+    const { toast, commands } = parseState(state);
+    expect(toast).toBe("Wizard rolled initiative with disadvantage!");
+    expect(commands).toStrictEqual(["/em : Wizard rolls initiative with disadvantage", "/roll 2d20kl1"]);
+  });
+
+  test("Fighter rolls Club damage (hidden)", () => {
+    const state = {
+      click: { className: classes.rollWeaponDamage, event: {}, data: { name: "Club", damage: "1d8" } },
+      visible: false,
+      character: "Fighter",
+    };
+    const { toast, commands } = parseState(state);
+    expect(toast).toBe("Fighter rolled Club damage! (hidden)");
+    expect(commands).toStrictEqual(["/w gm Fighter rolls Club damage", "/gmroll 1d8"]);
+  });
+});
