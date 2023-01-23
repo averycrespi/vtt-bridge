6e4592fc767601ba0617735a61ffbbacc19aaad1
Garemat <aidan.morgansayce@gmail.com>
2023-01-23 11:45:30 +0000

    Formatting and adding spell card description

--- src/transform/commands.js
+++ src/transform/commands.js
@@ -1,4 +1,4 @@
-export const makeD20Roll = (mod, character, name,{ hasAdvantage, hasDisadvantage, visible }) => {
+export const makeD20Roll = (mod, character, name, { hasAdvantage, hasDisadvantage, visible }) => {
   const prefix = visible ? "" : "/w gm ";
   const suffix = mod === "0" ? "" : mod;
   let rollType = ""
@@ -10,7 +10,7 @@ export const makeD20Roll = (mod, character, name,{ hasAdvantage, hasDisadvantage
     rollType = "always"
   }
 
-  return prefix + "&{template:simple} {{charname="+ character+"}} {{rname="+name+"}} {{mod="+mod+"}} {{r1=[[1d20"+mod+"]]}} {{"+rollType+"=1}} {{r2=[[1d20"+mod+"]]}}"
+  return prefix + "&{template:simple} {{charname=" + character + "}} {{rname=" + name + "}} {{mod=" + mod + "}} {{r1=[[1d20" + mod + "]]}} {{" + rollType + "=1}} {{r2=[[1d20" + mod + "]]}}"
 };
 
 export const makeDamageRoll = (damage, { visible }) => {
@@ -18,9 +18,35 @@ export const makeDamageRoll = (damage, { visible }) => {
   return prefix + damage;
 };
 
-export const makeDescription = (description, { visible }) => {
+export const makeDescription = (description, character, spellName, spellItems, { visible }) => {
   const prefix = visible ? "" : "/w gm ";
-  return prefix + description.replace(/\n/g, "\n" + prefix);
+  const descriptionBlock = spellItems[4];
+  let components = "";
+  let materials = "";
+
+  //Split out the casting components and material components of spell description
+  //As of writing this there are NO spells with only material components, RAW requires an open hand to handle material components, so will always be at least S,M, so didn't write logic to handle only material componets
+
+  if (descriptionBlock.length > 15) { //If the spell card contains material components
+    components = descriptionBlock.substr(11, descriptionBlock.length).split(",")
+    const materialsArray = components[components.length - 1].split(" ");
+    components.splice(-1);
+    components.push(materialsArray[1])
+    materialsArray.splice(1, 1);
+    materials = materialsArray.join(" ").slice(2,-1); //The slice is to remove the first and last bracket since roll20 template adds them in
+  } else if (descriptionBlock.length > 12) { // Is just V,S
+    components = descriptionBlock.substr(11, 15)
+  } else { //Only V or S
+    components = descriptionBlock.substr(11, 12)
+  }
+
+  //Work out if the spell as VSM for template 
+  const verbal = (components.includes("V") || components.includes(" V")) ? "1" : "0";
+  const somatic = (components.includes("S") || components.includes(" S") ) ? "1" : "0";
+  const material = (components.includes("M") || components.includes(" M")) ? "1" : "0";
+
+
+  return prefix + "&{template:spell} {{charname=" + character + "}} {{name=" + spellName + "}} {{castingtime=" + spellItems[1].substr(13, spellItems[1].length) + "}} {{range=" + spellItems[2] + "}} {{duration=" + spellItems[3] + "}} {{level=" + spellItems[0] + "}} {{v="+verbal+ "}} {{s="+somatic+ "}} {{m="+material+ "}} {{material=" + materials + "}} {{description=" + description.replace(/\n/g, "\n") + "}}"
 };
 
 export const makeEmote = (text, { hasAdvantage, hasDisadvantage, visible }) => {
@@ -48,5 +74,14 @@ export const makeWeaponAttack = (attack, character, wepName, damage, { hasAdvant
     rollType = "always"
   }
 
-  return prefix + "&{template:atkdmg} {{charname="+character+"}} {{rname="+wepName+"}} {{mod=+"+diceRoll[1]+"}} {{r1=[[1d20 +"+diceRoll[1]+"]]}} {{attack=1}} {{damage=1}} {{dmg1flag=1}} {{dmg1=[["+damage+"]]}} {{dmg1type=Piercing}} {{crit1=Crit: [[1d4]]}} {{"+rollType+"=1}} {{r2=[[1d20 +"+diceRoll[1]+"]]}}";
+  return prefix + "&{template:atkdmg} {{charname=" + character + "}} {{rname=" + wepName + "}} {{mod=+" + diceRoll[1] + "}} {{r1=[[1d20 +" + diceRoll[1] + "]]}} {{attack=1}} {{damage=1}} {{dmg1flag=1}} {{dmg1=[[" + damage + "]]}} {{dmg1type=Piercing}} {{crit1=Crit: [[1d4]]}} {{" + rollType + "=1}} {{r2=[[1d20 +" + diceRoll[1] + "]]}}";
+};
+
+export const makeSpellAttack = (attack, { hasAdvantage, hasDisadvantage, visible }) => {
+  const prefix = visible ? "/roll " : "/gmroll ";
+  if (hasAdvantage || hasDisadvantage) {
+    return prefix + attack + "\n" + prefix + attack;
+  } else {
+    return prefix + attack;
+  }
 };
