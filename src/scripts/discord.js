a1e94bf6a70a31383f4ec561ff7683619a3afaf5
Garemat <aidan.morgansayce@gmail.com>
2023-01-26 22:35:21 +0000

    Formatted webhook a bit

--- src/scripts/discord.js
+++ src/scripts/discord.js
@@ -2,7 +2,7 @@ export function sendMessage(message) {
   chrome.storage.sync.get("discordurl", function (items) {
     if (!chrome.runtime.error) {
       console.debug('Loaded: URL as' + items.discordurl);
-
+      const character = document.querySelector(".character-name").innerText
       const request = new XMLHttpRequest();
       request.open("POST", items.discordurl);
 
@@ -10,8 +10,15 @@ export function sendMessage(message) {
 
       const params = {
         username: "DMZ-Bot",
-        avatar_url: "",
-        content: message
+        avatar_url: "https://github.com/Garemat/vtt-bridge/blob/main/icons/128.png?raw=true",
+        content: null, 
+        embeds: [
+          {
+          title: character, 
+          description: character + " has made a " + message
+        }
+        ]
+        
       }
 
       request.send(JSON.stringify(params));
